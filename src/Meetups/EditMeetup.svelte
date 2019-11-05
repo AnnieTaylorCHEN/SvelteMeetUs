beddd0147e445d81adad2b088cec4fa79751bd67
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-11-05 21:32:29 +0100

    add store and finish app functions.

--- src/Meetups/EditMeetup.svelte
+++ src/Meetups/EditMeetup.svelte
@@ -1,8 +1,12 @@
 <script>
+  import meetups from "./meetups-store.js";
   import { createEventDispatcher } from "svelte";
   import TextInput from "../UI/TextInput.svelte";
   import Button from "../UI/Button.svelte";
   import Modal from "../UI/Modal.svelte";
+  import { isEmpty, isValidEmail } from "../helpers/validation.js";
+
+  export let id = null;
 
   let title = "";
   let subtitle = "";
@@ -11,21 +15,62 @@
   let description = "";
   let imageUrl = "";
 
+  if (id) {
+    const unsubscribe = meetups.subscribe(items => {
+      const selectedMeetup = items.find(i => i.id === id);
+      title = selectedMeetup.title;
+      subtitle = selectedMeetup.subtitle;
+      address = selectedMeetup.address;
+      email = selectedMeetup.contactEmail;
+      description = selectedMeetup.description;
+      imageUrl = selectedMeetup.imageUrl;
+    });
+
+    unsubscribe();
+  }
+
   const dispatch = createEventDispatcher();
 
+  $: titleValid = !isEmpty(title);
+  $: subtitleValid = !isEmpty(subtitle);
+  $: addressValid = !isEmpty(address);
+  $: descriptionValid = !isEmpty(description);
+  $: imageUrlValid = !isEmpty(imageUrl);
+  $: emailValid = isValidEmail(email);
+  $: formIsValid =
+    titleValid &&
+    subtitleValid &&
+    addressValid &&
+    descriptionValid &&
+    imageUrlValid &&
+    emailValid;
+
   function submitForm() {
-    dispatch("save", {
+    const meetupData = {
       title: title,
       subtitle: subtitle,
-      address: address,
-      email: email,
       description: description,
-      imageUrl: imageUrl
-    });
+      imageUrl: imageUrl,
+      contactEmail: email,
+      address: address
+    };
+
+    // meetups.push(newMeetup); // DOES NOT WORK!
+    if (id) {
+      meetups.updateMeetup(id, meetupData);
+    } else {
+      meetups.addMeetup(meetupData);
+    }
+    dispatch("save");
+  }
+
+  function deleteMeetup() {
+    meetups.removeMeetup(id);
+    dispatch("save");
   }
 
   function cancel() {
-      dispatch('cancel');
+    dispatch("cancel");
   }
 </script>
 
@@ -36,42 +81,58 @@
 </style>
 
 <Modal title="Edit Meetup Data" on:cancel>
-  <form on:submit|preventDefault={submitForm}>
+  <form on:submit={submitForm}>
     <TextInput
       id="title"
       label="Title"
+      valid={titleValid}
+      validityMessage="Please enter a valid title."
       value={title}
       on:input={event => (title = event.target.value)} />
     <TextInput
       id="subtitle"
       label="Subtitle"
+      valid={subtitleValid}
+      validityMessage="Please enter a valid subtitle."
       value={subtitle}
       on:input={event => (subtitle = event.target.value)} />
     <TextInput
       id="address"
       label="Address"
+      valid={addressValid}
+      validityMessage="Please enter a valid address."
       value={address}
       on:input={event => (address = event.target.value)} />
     <TextInput
       id="imageUrl"
       label="Image URL"
+      valid={imageUrlValid}
+      validityMessage="Please enter a valid image url."
       value={imageUrl}
       on:input={event => (imageUrl = event.target.value)} />
     <TextInput
       id="email"
       label="E-Mail"
       type="email"
+      valid={emailValid}
+      validityMessage="Please enter a valid email address."
       value={email}
       on:input={event => (email = event.target.value)} />
     <TextInput
       id="description"
       label="Description"
       controlType="textarea"
-      value={description}
-      on:input={event => (description = event.target.value)} />
+      valid={descriptionValid}
+      validityMessage="Please enter a valid description."
+      bind:value={description} />
   </form>
   <div slot="footer">
     <Button type="button" mode="outline" on:click={cancel}>Cancel</Button>
-    <Button type="button" on:click={submitForm}>Save</Button>
+    <Button type="button" on:click={submitForm} disabled={!formIsValid}>
+      Save
+    </Button>
+    {#if id}
+      <Button type="button" on:click={deleteMeetup}>Delete</Button>
+    {/if}
   </div>
 </Modal>
